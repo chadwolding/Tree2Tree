2ed81d503994aa5df55cd7e03c333b2336c15a36
Trzebiatowski Isaac <081104@MacBook-Pro.localdomain>
2018-05-23 23:10:48 -0500

    seting up a repo

---
+++ javascript/javascript.js
@@ -0,0 +1,45 @@
+var fullPageMenu = document.getElementById('fullPageMenu');
+
+/* Modal Image on gallery Page*/
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("modalImage");
+var captionText = document.getElementById("caption");
+
+
+function toggleNavPage() {
+
+  var width = document.getElementById('fullPageMenu').style.width;
+  if (width == "" || width == "0%") {
+    document.getElementById('fullPageMenu').style.width = "215px";
+  } else {
+    document.getElementById('fullPageMenu').style.width = "0%";
+  }
+}
+
+function closeNavPage() {
+  document.getElementById('fullPageMenu').style.width = "0%";
+}
+
+window.onclick = function(event) {
+  if (event.target == fullPageMenu) {
+    closeNavPage();
+  }
+}
+
+window.onresize = function() {
+  if (window.innerWidth >= 815) {
+    document.getElementById('fullPageMenu').style.width = "0%";
+  }
+}
+
+/* Modal Image on gallery Page*/
+function fullScreen(id) {
+    "use strict";
+    modal.style.display = "block";
+    modalImg.src = "images/" + id + ".png";
+}
+
+function pleaseClose() {
+    "use strict";
+    modal.style.display = "none";
+}
